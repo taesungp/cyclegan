6178d564804bfa1d4b7daf6df96c6cb696a9d192
Taesung Park <taesung_park@berkeley.edu>
2017-03-26 22:52:44 -0700

    fixed bugs in gatys comparison

--- create_monet2photo_table.py
+++ create_monet2photo_table.py
@@ -86,7 +86,7 @@ def show_gatys_comparison():
   artists = ['vangogh', 'ukiyoe','cezanne','monet']  
   for index in indices[:30]:
     for artist in artists:
-      print("| %s | ![]({{site.baseurl}}/images/%s/%d_content.png) | ![]({{site.baseurl}}/images/%s/resized_128/%d_style_%s_0.png) | ![]({{site.baseurl}}/images/%s/%d_result_%s_0.png) |![]({{site.baseurl}}/images/%s/resized_128/%d_style_%s_1.png) | ![]({{site.baseurl}}/images/%s/%d_result_%s_1.png) | ![]({{site.baseurl}}/images/%s/%d_result_%s_total.png) | ![]({{site.baseurl}}/images/%s/%s/%d.jpg) |" % 
+      print("| %s | ![]({{site.baseurl}}/images/%s/%d_content.jpg) | ![]({{site.baseurl}}/images/%s/resized_128/%d_style_%s_0.jpg) | ![]({{site.baseurl}}/images/%s/%d_result_%s_0.jpg) |![]({{site.baseurl}}/images/%s/resized_128/%d_style_%s_1.jpg) | ![]({{site.baseurl}}/images/%s/%d_result_%s_1.jpg) | ![]({{site.baseurl}}/images/%s/%d_result_%s_total.jpg) | ![]({{site.baseurl}}/images/%s/%s/%d.jpg) |" % 
             (artist, dir_name, index, 
              dir_name, index, artist,
              dir_name, index, artist,
